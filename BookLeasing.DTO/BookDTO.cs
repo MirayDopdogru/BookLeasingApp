7a3e8badc5769dc58efd353f6c494d753e15ea1a
Miray <miraydopdogru@gmail.com>
2019-06-04 13:52:52 +0300

    Add project files.

---
+++ BookLeasing.DTO/BookDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLeasing.DTO
+{
+    public class BookDTO
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string PublisherName { get; set; }
+        public int PublishingYear { get; set; }
+        public int PageCount { get; set; }
+        public string Summary { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
